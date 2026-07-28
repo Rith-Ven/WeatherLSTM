37b040518b7f062b05ad581f949de8836436c7ca
Rith-Ven <rithanya.venkatesh16@gmail.com>
2026-07-27 20:40:05 -0400

    training and predictions

--- weather_lstm.py
+++ weather_lstm.py
@@ -10,7 +10,7 @@ from keras.layers import LSTM, Dense, Dropout   # Recurrent layer to learn tempo
 np.random.seed(42)
 tf.random.set_seed(42)
 
-# Load and clean the csv data
+# **Load and clean the csv data
 print("Loading Charlotte weather data...")
 df = pd.read_csv("charlotte-weather.csv")
 
@@ -22,11 +22,11 @@ data = df[['DATE', 'TAVG']].copy() # Creates a new dataframe with JUST data and 
 
 print(f"Data successfully loaded. Date range: {data['DATE'].min().strftime('%Y-%m-%d')} to {data['DATE'].max().strftime('%Y-%m-%d')}")
 
-# Data scaling
+# **Data scaling
 scaler = MinMaxScaler(feature_range=(0,1)) # Standardizer
 scaled_temp = scaler.fit_transform(data[['TAVG']]) # Finds global min and max and applies scaling x_scaled = (x-x_min) / (x_max - x_min)
 
-# Creating a sequence 
+# **Creating a sequence 
 WINDOW_SIZE = 14 # Model inspects 14 consecutive past days (t-14) to predict temp for day t
 
 # Iterates through scaled dataset to build pairs of input sequences (X) and target outputs (y)
@@ -45,7 +45,7 @@ X_train, X_test = X[:train_size], X[train_size:]
 y_train, y_test = y[:train_size], y[train_size:]
 dates_test = data['DATE'].iloc[WINDOW_SIZE + train_size:].reset_index(drop = True) #Gets calendar dates for test predictions for accurate time-series plotting
 
-# Building LSTM Network Architecture
+# **Building LSTM Network Architecture
 # Sequential lets data flow through stacked layers
 model = Sequential([ 
     # Has 64 memory units, outputs a hidden state sequence across 14 time steps, lets next layer analyze temporal patterns
@@ -64,4 +64,20 @@ model = Sequential([
 # Tracks Mean Absolute Error during training
 model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['mae'])
 model.summary()
+
+# **Training the model
+print("\nTraining the LSTM model...")
+# Training loop across training pairs, completes 25 full forward backward training passes, updates weights every 32 pairs, computes validation loss
+history = model.fit(X_train, y_train, epochs = 25, batch_size = 32, validation_data = (X_test, y_test), verbose = 1)
+
+# **Generating predictions
+print("\nGenerating predictions on test set...")
+predictions_scaled = model.predict(X_test)
+# Converts normalized outputs back into readable degrees
+predictions = scaler.inverse_transform(predictions_scaled)
+y_test_actual = scaler.inverse_transform(y_test)
+
+# Calculates final performance, MAE measures average scalar pred error magnitude and RMSE penalizes larger localized pred errors
+mae = np.mean(np.abs(predictions - y_test_actual))
+rmse = np.sqrt(np.mean((predictions - y_test_actual) **2))
 
